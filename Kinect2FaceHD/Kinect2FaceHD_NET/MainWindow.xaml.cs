3f9bea9583aaae9075ba97867864a8fb63e6e513
Vangos <pterneas@gmail.com>
2015-06-04 01:29:28 +0300

    Added .NET 4.5 and WinRT projects.

---
+++ Kinect2FaceHD/Kinect2FaceHD_NET/MainWindow.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Face;
+
+namespace Kinect2FaceHD_NET
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectSensor _sensor = null;
+
+        private BodyFrameSource _bodySource = null;
+
+        private BodyFrameReader _bodyReader = null;
+
+        private HighDefinitionFaceFrameSource _faceSource = null;
+
+        private HighDefinitionFaceFrameReader _faceReader = null;
+
+        private FaceAlignment _faceAlignment = null;
+
+        private FaceModel _faceModel = null;
+
+        private List<Ellipse> _points = new List<Ellipse>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _sensor = KinectSensor.GetDefault();
+
+            if (_sensor != null)
+            {
+                _bodySource = _sensor.BodyFrameSource;
+                _bodyReader = _bodySource.OpenReader();
+                _bodyReader.FrameArrived += BodyReader_FrameArrived;
+
+                _faceSource = new HighDefinitionFaceFrameSource(_sensor);
+
+                _faceReader = _faceSource.OpenReader();
+                _faceReader.FrameArrived += FaceReader_FrameArrived;
+
+                _faceModel = new FaceModel();
+                _faceAlignment = new FaceAlignment();
+                
+                _sensor.Open();
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_faceModel != null)
+            {
+                _faceModel.Dispose();
+                _faceModel = null;
+            }
+
+            GC.SuppressFinalize(this);
+        }
+        
+        private void BodyReader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            using (var frame = e.FrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    Body[] bodies = new Body[frame.BodyCount];
+                    frame.GetAndRefreshBodyData(bodies);
+
+                    Body body = bodies.Where(b => b.IsTracked).FirstOrDefault();
+
+                    if (!_faceSource.IsTrackingIdValid)
+                    {
+                        if (body != null)
+                        {
+                            _faceSource.TrackingId = body.TrackingId;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void FaceReader_FrameArrived(object sender, HighDefinitionFaceFrameArrivedEventArgs e)
+        {
+            using (var frame = e.FrameReference.AcquireFrame())
+            {
+                if (frame != null && frame.IsFaceTracked)
+                {
+                    frame.GetAndRefreshFaceAlignmentResult(_faceAlignment);
+                    UpdateFacePoints();
+                }
+            }
+        }
+
+        private void UpdateFacePoints()
+        {
+            if (_faceModel == null) return;
+
+            var vertices = _faceModel.CalculateVerticesForAlignment(_faceAlignment);
+
+            if (vertices.Count > 0)
+            {
+                if (_points.Count == 0)
+                {
+                    for (int index = 0; index < vertices.Count; index++)
+                    {
+                        Ellipse ellipse = new Ellipse
+                        {
+                            Width = 2.0,
+                            Height = 2.0,
+                            Fill = new SolidColorBrush(Colors.Blue)
+                        };
+
+                        _points.Add(ellipse);
+                    }
+
+                    foreach (Ellipse ellipse in _points)
+                    {
+                        canvas.Children.Add(ellipse);
+                    }
+                }
+
+                for (int index = 0; index < vertices.Count; index++)
+                {
+                    CameraSpacePoint vertice = vertices[index];
+                    DepthSpacePoint point = _sensor.CoordinateMapper.MapCameraPointToDepthSpace(vertice);
+
+                    if (float.IsInfinity(point.X) || float.IsInfinity(point.Y)) return;
+
+                    Ellipse ellipse = _points[index];
+
+                    Canvas.SetLeft(ellipse, point.X);
+                    Canvas.SetTop(ellipse, point.Y);
+                }
+            }
+        }
+    }
+}
---
+++ Kinect2FaceHD/Kinect2FaceHD_WinRT/MainPage.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+using WindowsPreview.Kinect;
+using Microsoft.Kinect.Face;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Kinect2FaceHD_WinRT
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private KinectSensor _sensor = null;
+
+        private BodyFrameSource _bodySource = null;
+
+        private BodyFrameReader _bodyReader = null;
+
+        private HighDefinitionFaceFrameSource _faceSource = null;
+
+        private HighDefinitionFaceFrameReader _faceReader = null;
+
+        private FaceAlignment _faceAlignment = null;
+
+        private FaceModel _faceModel = null;
+
+        private List<Ellipse> _points = new List<Ellipse>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _sensor = KinectSensor.GetDefault();
+
+            if (_sensor != null)
+            {
+                _bodySource = _sensor.BodyFrameSource;
+                _bodyReader = _bodySource.OpenReader();
+                _bodyReader.FrameArrived += BodyReader_FrameArrived;
+
+                _faceSource = new HighDefinitionFaceFrameSource(_sensor);
+
+                _faceReader = _faceSource.OpenReader();
+                _faceReader.FrameArrived += FaceReader_FrameArrived;
+
+                _faceModel = new FaceModel();
+                _faceAlignment = new FaceAlignment();
+
+                _sensor.Open();
+            }
+        }
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (_faceModel != null)
+            {
+                _faceModel.Dispose();
+                _faceModel = null;
+            }
+
+            GC.SuppressFinalize(this);
+        }
+
+        private void BodyReader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            using (var frame = e.FrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    Body[] bodies = new Body[frame.BodyCount];
+                    frame.GetAndRefreshBodyData(bodies);
+
+                    Body body = bodies.Where(b => b.IsTracked).FirstOrDefault();
+
+                    if (!_faceSource.IsTrackingIdValid)
+                    {
+                        if (body != null)
+                        {
+                            _faceSource.TrackingId = body.TrackingId;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void FaceReader_FrameArrived(object sender, HighDefinitionFaceFrameArrivedEventArgs e)
+        {
+            using (var frame = e.FrameReference.AcquireFrame())
+            {
+                if (frame != null && frame.IsFaceTracked)
+                {
+                    frame.GetAndRefreshFaceAlignmentResult(_faceAlignment);
+                    UpdateFacePoints();
+                }
+            }
+        }
+
+        private void UpdateFacePoints()
+        {
+            if (_faceModel == null) return;
+
+            var vertices = _faceModel.CalculateVerticesForAlignment(_faceAlignment);
+
+            if (vertices.Count > 0)
+            {
+                if (_points.Count == 0)
+                {
+                    for (int index = 0; index < vertices.Count; index++)
+                    {
+                        Ellipse ellipse = new Ellipse
+                        {
+                            Width = 2.0,
+                            Height = 2.0,
+                            Fill = new SolidColorBrush(Colors.Blue)
+                        };
+
+                        _points.Add(ellipse);
+                    }
+
+                    foreach (Ellipse ellipse in _points)
+                    {
+                        canvas.Children.Add(ellipse);
+                    }
+                }
+
+                for (int index = 0; index < vertices.Count; index++)
+                {
+                    CameraSpacePoint vertice = vertices[index];
+                    DepthSpacePoint point = _sensor.CoordinateMapper.MapCameraPointToDepthSpace(vertice);
+
+                    if (float.IsInfinity(point.X) || float.IsInfinity(point.Y)) return;
+
+                    Ellipse ellipse = _points[index];
+
+                    Canvas.SetLeft(ellipse, point.X);
+                    Canvas.SetTop(ellipse, point.Y);
+                }
+            }
+        }
+    }
+}
